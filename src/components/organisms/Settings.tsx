a2db038652efde11cb4262add54b3c48af7dfe42
Vladislav Klyukin <vk.azeria@gmail.com>
2024-05-28 23:32:03 +0500

    change size of the settings button

--- src/components/organisms/Settings.tsx
+++ src/components/organisms/Settings.tsx
@@ -3,7 +3,7 @@ import { ReactComponent as SettingsIcon } from "@assets/settings.svg";
 export const Settings = () => {
   return (
     <div data-testid="settings">
-      <button className="flex size-12 place-items-center">
+      <button className="flex size-10 place-items-center">
         <SettingsIcon className="size-full fill-main dark:fill-dark-main" />
       </button>
     </div>
