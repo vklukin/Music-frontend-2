fbd771e82c139436dff04772e616755d93611bd6
Vladislav Klyukin <vk.azeria@gmail.com>
2024-05-28 23:31:27 +0500

    add chadow on the header

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -5,7 +5,7 @@ import { Settings } from "@components/organisms/Settings";
 
 export function Header() {
   return (
-    <header className="flex h-10 w-full tablet:h-[60px]">
+    <header className="flex h-10 w-full shadow-md shadow-main tablet:h-[60px] dark:shadow-dark-main">
       <PageWidthContainer styles="flex justify-between items-center">
         <div className="flex h-full flex-row items-center gap-8 tablet:gap-24">
           <Logo containerStyles="h-[80%]" />
