7c2ea7ab3cc3ed997b4dc945717836ad17bc983f
Vladislav Klyukin <vk.azeria@gmail.com>
2024-06-23 17:31:58 +0500

    changed router position

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { BrowserRouter } from "react-router-dom";
+
 import { Router } from "./Router";
+import { Header } from "@components/organisms/Header";
 
 import { useAppHooks } from "./App.hooks";
+import "./app.css";
 
 function App() {
   useAppHooks();
 
-  return <Router />;
+  return (
+    <div className="layout layout--main">
+      <Header />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 
-import { MainLayout } from "@components/templates/MainLayout";
-
 export function Router() {
   return (
     <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<></>} />
-      </Route>
+      <Route path="/" element={<></>} />
     </Routes>
   );
 }
