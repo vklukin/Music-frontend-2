c0439aa12affd92623c6cd48db7811859b6ce9a3
Vladislav Klyukin <vk.azeria@gmail.com>
2024-07-27 16:36:48 +0500

    created player controll button components

---
+++ src/components/atoms/PlayerControlButton/PlayerControlButton.tsx
@@ -0,0 +1,31 @@
+import cn from "classnames";
+
+import "./style.css";
+
+import { ReactComponent as Pause } from "@assets/pause.svg";
+import { ReactComponent as Play } from "@assets/play.svg";
+import { ReactComponent as Back } from "@assets/skip-back.svg";
+import { ReactComponent as Forward } from "@assets/skip-forward.svg";
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  iconType: "prev" | "pause" | "play" | "next";
+};
+
+export const PlayerControlButton = (props: ButtonProps) => {
+  const { iconType } = props;
+
+  const Icon: {
+    [T in ButtonProps["iconType"]]: React.ReactNode;
+  } = {
+    prev: <Back />,
+    next: <Forward />,
+    pause: <Pause />,
+    play: <Play />
+  };
+
+  return (
+    <button className={cn("player-control-button", props.className)} {...props}>
+      {Icon[iconType]}
+    </button>
+  );
+};
---
+++ src/components/atoms/PlayerControlButton/index.ts
@@ -0,0 +1,1 @@
+export { PlayerControlButton } from "./PlayerControlButton";
