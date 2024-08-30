fbdfe3bb4de8769ae14a653a2106b0180348263b
Vladislav Klyukin <vk.azeria@gmail.com>
2024-08-30 23:02:16 +0500

    added api wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 
 import App from "./App.tsx";
+import { ApiWrapper } from "./components/templates/ApiWrapper/ApiWrapper.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLDivElement).render(
-  <App />
+  <ApiWrapper>
+    <App />
+  </ApiWrapper>
 );
