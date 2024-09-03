8ea9d8b51d483d16d3393492d98a5b177271b6de
Vladislav Klyukin <vk.azeria@gmail.com>
2024-09-03 20:59:37 +0500

    fixed axios definition

--- src/core/configs/api.config.ts
+++ src/core/configs/api.config.ts
@@ -1,11 +1,13 @@
-import { Axios } from "axios";
+import Axios from "axios";
 
 import { apiURL, apiTimeout } from "../constants/api";
 
-export const Api = new Axios({
+export const Api = Axios.create({
   baseURL: apiURL,
   headers: {
+    Accept: "application/json",
     "Content-Type": "application/json"
   },
+  responseType: "json",
   timeout: apiTimeout
 });
